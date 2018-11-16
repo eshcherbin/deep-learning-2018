d2112b1335253d17aa27a0ddf92c4df90967bdb9
Egor Shcherbin <javamanse@ya.ru>
2018-11-16 19:32:34 +0300

    Changed import for colab

--- 89/homework/train_dcgan.py
+++ 89/homework/train_dcgan.py
@@ -7,8 +7,8 @@ import torchvision.datasets as datasets
 from torch.optim import Adam
 from torchvision import transforms
 
-from homework.dcgan.dcgan import DCGenerator, DCDiscriminator
-from homework.dcgan.trainer import DCGANTrainer
+from dcgan.dcgan import DCGenerator, DCDiscriminator
+from dcgan.trainer import DCGANTrainer
 
 
 def get_config():
